5d5bdaa82439add600e6aa7b6c4eb9fc04091255
lazaromasot <lazz84@hotmail.com>
2023-04-12 22:49:18 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    alert("Loading weather report...");
+}
+
+document.getElementById("myButton").onclick;
+
+function removeMessage() {
+    var element = document.getElementById("message");
+    element.remove();
+}
